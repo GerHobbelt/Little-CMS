5f5a1d12b7b88934d3204f6c452077c7f2179d5b
Ger Hobbelt <ger@hobbelt.com>
2023-09-06 19:31:25 +0200

    fix whitespace + typos in comments
    # Conflicts:
    #	plugins/fast_float/src/fast_8_matsh.c
    #	plugins/fast_float/src/fast_8_matsh_sse.c
    #	plugins/fast_float/src/fast_float_15mats.c
    #	plugins/fast_float/src/fast_float_matsh.c

--- plugins/fast_float/src/fast_float_15mats.c
+++ plugins/fast_float/src/fast_float_15mats.c
@@ -331,8 +331,8 @@ cmsBool OptimizeMatrixShaper15(cmsContext ContextID,
               _cmsStageToneCurvesData* mpeC1 = (_cmsStageToneCurvesData*)cmsStageData(ContextID, Curve1);
               _cmsStageToneCurvesData* mpeC2 = (_cmsStageToneCurvesData*)cmsStageData(ContextID, Curve2);
 
-              // In this particular optimization, caché does not help as it takes more time to deal with
-              // the cache that with the pixel handling
+              // In this particular optimization, cache does not help as it takes more time to deal with
+              // the cache than with the pixel handling
               *dwFlags |= cmsFLAGS_NOCACHE;
 
               // Setup the optimizarion routines
