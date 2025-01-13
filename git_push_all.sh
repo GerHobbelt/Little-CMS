9ff6f26a1a54f089d238161434c05b25f359df58
Ger Hobbelt <ger@hobbelt.com>
2025-01-13 01:04:48 +0100

    fix the little bash/shell script to help push this repo to *BOTH* tracking forks of mine: see also previous commit.

--- git_push_all.sh
+++ git_push_all.sh
@@ -6,16 +6,10 @@ cat <<EOF
 >>> Pushing our work to both remotes...
 
 EOF
-for f in GerHobbelt/thirdpart-lcms2 GerHobbelt/Little-CMS ; do 
-	echo $f 
-	git push --all git@github.com:$f.git
+for f in GerHobbelt/thirdparty-lcms2 GerHobbelt/Little-CMS ; do
+    echo ""
+    echo "::REPO: $f"
+    git push --all --follow-tags git@github.com:$f.git                                         2>&1
+    git push --tags              git@github.com:$f.git                                         2>&1
 done
 
-# fetch all remote work as an afterthought
-cat <<EOF
-
-
->>> Fetching all remote work...
-
-EOF
-git fetch --all 
